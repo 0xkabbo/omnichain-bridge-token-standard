e2add318505babecf7a724d08cea5e4057b50b32
Kabbo <161012310+0xkabbo@users.noreply.github.com>
2026-03-20 21:11:16 +0600

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,14 @@
+// LayerZero Endpoint IDs for various networks
+module.exports = {
+    ENDPOINTS: {
+        ETHEREUM: "101",
+        BNB_CHAIN: "102",
+        ARBITRUM: "110",
+        OPTIMISM: "111",
+        POLYGON: "109",
+        BASE: "184"
+    },
+    GAS_LIMITS: {
+        TRANSFER: 200000
+    }
+};
